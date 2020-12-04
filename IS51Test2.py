871de2139b9abf2230d720b8a6ca0ee166c7158a
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-12-03 16:11:39 -0800

    Structured English finished

--- IS51Test2.py
+++ IS51Test2.py
@@ -1,4 +1,9 @@
 """
+first we will read the numbers in number.txt
+assign each number into  a list
+calculate the average of the grades
+then we will see what percentage of grades are above the average
+
 
 
 """
