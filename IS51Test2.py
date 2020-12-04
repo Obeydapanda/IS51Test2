0a244dd652c61a8c3d5ff6d8507b8cb3219f5d86
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-12-03 16:41:26 -0800

    Finished Psuedo Code

--- IS51Test2.py
+++ IS51Test2.py
@@ -1,6 +1,43 @@
 """
 
 
+main():
+    infile = open("numbers.txt", "r")
+    grades = [word.strip() for word in infile]
+    infile.close()
+    calculate_average()
+    calculate_percent_above_average(grades)
+
+    print ("Number of Grades:", len(grades))
+    print ("Average Grade:", calculate_average(grades))
+    print ("Percentage of grades above average:", calculate_percent_above_average(grades), "%")
+    
+
+calculate_percent_above_average(grades):
+    avg = calculate_average(grades)
+    counter = 0
+    for grade in grades:
+        if grade > avg:
+            counter += 1
+
+
+    return (counter/len(grades))
+
+
+    
+
+calculate_average(grades)
+    if len(grades) == 0:
+        return 0
+    
+    avg = sum(grades)/len(grades)
+    
+    return avg
+
+
+
+main()
+
 """
 
 """
