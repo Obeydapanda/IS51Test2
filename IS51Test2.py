03dbf74fb24b4ad212dc5029bfe24371b3d243b9
Obeydapanda <rmbancoro@mail.fresnostate.edu>
2020-12-03 17:00:43 -0800

    Revised Finished Structured-English

--- IS51Test2.py
+++ IS51Test2.py
@@ -3,6 +3,7 @@ first we will read the numbers in number.txt
 assign each number into  a list
 calculate the average of the grades
 then we will see what percentage of grades are above the average
+print out the values for # of grades ,average, and # of people above average
 
 
 
